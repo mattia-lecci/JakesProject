b2c4417d62107af2cec9bba19c691bb1154817dc
mattia-lecci <mattia.lecci@gmail.com>
2017-08-01 17:11:16 +0200

    Jakes simulator complete (ref A2)

--- Code/Channels/createChannel.m
+++ Code/Channels/createChannel.m
@@ -1,19 +1,21 @@
-function ch = createChannel(fd,T,duration,varargin)
+function [ch,t] = createChannel(fd,T,duration,varargin)
 % arg check
 p = inputParser;
 inputCheck();
 
 %% Extracting inputs
 simulator = p.Results.simulator;
+nSin = p.Results.nSin;
 DurationType = p.Results.DurationType;
 NChannels = p.Results.NChannels;
 
 %% Creating channel
 [sampleDuration,timeDuration] = getDuration(T,duration,DurationType);
+t = (0:T:timeDuration)'; % time vector (column)
 
 switch simulator
     case 'Jakes'
-        ch = JakesSimulator(fd,T,timeDuration,NChannels);
+        ch = JakesSimulator(fd,t,nSin,NChannels);
     otherwise
         ch = 0;
 end
@@ -31,6 +33,8 @@ end
             @(x)validateattributes(x,{'numeric'},{'scalar','nonnegative'}));
         p.addOptional('simulator','Jakes',...
             @(x)any(validatestring(x,simulatorsList)))
+        p.addOptional('nSin',10,...
+            @(x)validateattributes(x,{'numeric'},{'scalar','positive','integer'}));
         p.addParameter('DurationType','time',...
             @(x)any(validatestring(x,{'time','samples'})));
         p.addParameter('NChannels',1,...
@@ -46,7 +50,7 @@ function [sampleDuration,timeDuration] = getDuration(T,duration,durationType)
 switch durationType
     case 'time'
         timeDuration = duration;
-        sampleDuration = floor(duration/T);
+        sampleDuration = floor(duration/T)+1;
     case 'samples'
         sampleDuration = duration;
         timeDuration = T*duration;
