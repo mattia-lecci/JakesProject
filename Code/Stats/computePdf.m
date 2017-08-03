d54c993f3ddd16f520993eb27e7a77080e70949b
mattia-lecci <mattia.lecci@gmail.com>
2017-08-03 16:32:40 +0200

    Added binMethod argument to computePdf

--- Code/Stats/computePdf.m
+++ Code/Stats/computePdf.m
@@ -1,4 +1,4 @@
-function pdf = computePdf(ch,ind)
+function pdf = computePdf(ch,ind,binMethod)
 %COMPUTEPDF Computes pdfs of the given channel using as realizations the
 %columns of ch (which should be independent channels)
 % pdf = COMPUTEPDF(ch,ind) Computes pdf for channel ch on samples indexes
@@ -11,7 +11,12 @@ function pdf = computePdf(ch,ind)
 % - magnitude.edges: vector of histogram edges
 % - phse.normBinCount: vector of histogram bin count normalized as pdf
 % - phse.edges: vector of histogram edges
+% pdf = COMPUTEPDF(ch,ind,binMethod) Allows you to choose the binning
+% method of histcounts from: 'auto', 'scott', 'fd', 'integers', 'sturges',
+% 'sqrt' (default)
 %
+% NOTE: No uniform distribution fit is available in matlab, that's why
+% phase.fit is not present
 %
 % normBinCount and edges can be visualized with the following command:
 % histogram('BinEdges',edges,'BinCounts',normBinCount)
@@ -26,23 +31,26 @@ for i = N:-1:1 % to avoid preallocation
     % magnitude
     pdf(i).magnitude.fit = fitdist( abs(ch(ind(i),:)).', 'Rayleigh' );
     [pdf(i).magnitude.normBinCount,pdf(i).magnitude.edges] =...
-        histcounts( abs(ch(ind(i),:)), 'Normalization','pdf','BinMethod','sqrt');
+        histcounts( abs(ch(ind(i),:)), 'Normalization','pdf','BinMethod',binMethod);
     
     % phase
     [pdf(i).phase.normBinCount,pdf(i).phase.edges] =...
         histcounts( angle(ch(ind(i),:)), 'Normalization','pdf',...
-        'BinLimits',[-pi,pi],'BinMethod','sqrt');
+        'BinLimits',[-pi,pi],'BinMethod',binMethod);
 end
 
 %% Argument checker
     function inputCheck()
+        validBinMethods = {'auto','scott','fd','integers','sturges','sqrt'};
         
         p.addRequired('ch',...
             @(x)validateattributes(x,{'numeric'},{'nonempty'}));
         p.addRequired('ind',...
             @(x)validateattributes(x,{'numeric'},{'vector','positive','integer'}));
+        p.addOptional('binMethod','sqrt',...
+            @(x)any(validatestring(x,validBinMethods)));
         
-        p.parse(ch,ind);
+        p.parse(ch,ind,binMethod);
         
     end
 end
