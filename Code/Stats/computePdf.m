b4449dfc207f9d18b7e00058ee36adc8b1f62d73
mattia-lecci <mattia.lecci@gmail.com>
2017-08-03 16:20:51 +0200

    Added phase pdf, changed binning method

--- Code/Stats/computePdf.m
+++ Code/Stats/computePdf.m
@@ -1,18 +1,20 @@
 function pdf = computePdf(ch,ind)
-%COMPUTEPDF Computes pdf of the given channel using as realizations the
+%COMPUTEPDF Computes pdfs of the given channel using as realizations the
 %columns of ch (which should be independent channels)
 % pdf = COMPUTEPDF(ch,ind) Computes pdf for channel ch on samples indexes
 % given by ind (either scalar or vector). Returns a structure of the same
 % length of ind with fields:
-% - fit: containing a RayleighDistribution object fitted, from which you
-% can extract mean, variance, std, parameter (b) confidence interval, plot
-% pdf, cdf,...
-% - normBinCount: vector of histogram bin count normalized as pdf
-% - edges: vector of histogram edges
+% - magnitude.fit: containing a RayleighDistribution object fitted, from
+% which you can extract mean, variance, std, parameter (b) confidence
+% interval, plot pdf, cdf,...
+% - magnitude.normBinCount: vector of histogram bin count normalized as pdf
+% - magnitude.edges: vector of histogram edges
+% - phse.normBinCount: vector of histogram bin count normalized as pdf
+% - phse.edges: vector of histogram edges
 %
 %
 % normBinCount and edges can be visualized with the following command:
-% histogram('BinEdges',pdf.edges,'BinCounts',pdf.normBinCount)
+% histogram('BinEdges',edges,'BinCounts',normBinCount)
 
 % arg check
 p = inputParser;
@@ -21,9 +23,15 @@ inputCheck();
 %% fit pdf
 N = length(ind);
 for i = N:-1:1 % to avoid preallocation
-    pdf(i).fit = fitdist( abs(ch(ind(i),:)).', 'Rayleigh' );
-    [pdf(i).normBinCount,pdf(i).edges] = histcounts( abs(ch(ind(i),:)),...
-        'Normalization','pdf');
+    % magnitude
+    pdf(i).magnitude.fit = fitdist( abs(ch(ind(i),:)).', 'Rayleigh' );
+    [pdf(i).magnitude.normBinCount,pdf(i).magnitude.edges] =...
+        histcounts( abs(ch(ind(i),:)), 'Normalization','pdf','BinMethod','sqrt');
+    
+    % phase
+    [pdf(i).phase.normBinCount,pdf(i).phase.edges] =...
+        histcounts( angle(ch(ind(i),:)), 'Normalization','pdf',...
+        'BinLimits',[-pi,pi],'BinMethod','sqrt');
 end
 
 %% Argument checker
